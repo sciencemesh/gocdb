eceedc57d0f4c686a1585be003dbcd2511a90042
Daniel Müller <daniel.mueller@uni-muenster.de>
2021-07-20 16:35:09 +0200

    Login form updates

--- container/html/gocdb/htdocs/web_portal/login/index.php
+++ container/html/gocdb/htdocs/web_portal/login/index.php
@@ -91,13 +91,12 @@
 
         	xhr.onreadystatechange = function() {
         		if (this.readyState === XMLHttpRequest.DONE) {
+					var resp = JSON.parse(this.responseText);
+
         			if (this.status == 200) {
-        				setState(STATE_SUCCESS, "Your login was successful! Redirecting...");
-						// TODO: Extract & Store token
-						token = "1234";
-						 window.location.replace("login.php?token=" + token);
+						setState(STATE_SUCCESS, "Your login was successful! Redirecting...");
+						window.location.replace("login.php?token=" + resp.data);
         			} else {
-        				var resp = JSON.parse(this.responseText);
         				setState(STATE_ERROR, "An error occurred while trying to login your account:<br><em>" + resp.error + "</em>", "form", null, true);
         			}
                 }
